b3d82503dfd55db68078731945fea35960f80f86
Danny Nielsen <danny.nlsn0@gmail.com>
2017-11-01 20:39:05 +0100

    Finished user login functionality.

--- src/main/java/Routes/App.java
+++ src/main/java/Routes/App.java
@@ -1,9 +1,12 @@
 package Routes;
 
+import DataAccess.MongoDB;
 import io.swagger.annotations.Api;
+import org.bson.Document;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -27,9 +30,10 @@ public class App {
         return Response.ok().entity("Welcome!").status(200).build();
     }
 
-    @GET
+    @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response login(InputStream json) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(json));
         StringBuilder out = new StringBuilder();
@@ -52,7 +56,27 @@ public class App {
             ex.printStackTrace();
             return Response.status(400).entity(ex.getMessage()).build();
         }
+
+        Document userDoc = MongoDB.getUserDocument(id);
+        if(passwordMatches(userDoc, password)) {
+            String userJson = MongoDB.getUser(id);
+            return Response.status(200).entity(userJson).build();
+        }
+        else return Response.status(401).entity("Wrong username/password combination").build();
+
     }
+
+    /**
+     * Checks the input password against the decrypted password in the DB.
+     * @param userDoc User Document from database to check password on.
+     * @param inputPassword plain text input password.
+     * @return true, if passwords match. False, if they don't.
+     */
+    private Boolean passwordMatches(Document userDoc, String inputPassword){
+        String hashedPw = userDoc.getString("password");
+        return BCrypt.checkpw(inputPassword, hashedPw);
+    }
+
 
 
 }
